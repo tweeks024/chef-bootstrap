afb267622859176fa27203628079737ac7f3bd6a
tomw45362 <tom.m.weeks@gmail.com>
2015-09-01 09:31:35 -0400

    Added dhclient to cookbook.sh to fix name registration issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/chef-repo
-knife bootstrap localhost -x root -P pass -r 'recipe[$1]' >> /var/log/knife.log
-knife bootstrap localhost -x root -P pass -r 'recipe[bootstrap]' >> /var/log/knife.log
+dhclient -r && dhclient -H $HOSTNAME
+sleep 10
+knife bootstrap localhost -n $HOSTNAME -x root -P pass -r 'recipe[$1]' >> /var/log/knife.log
+knife bootstrap localhost -n $HOSTNAME -x root -P pass -r 'recipe[bootstrap]' >> /var/log/knife.log
