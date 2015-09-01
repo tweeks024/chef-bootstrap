e44a8a3c232b969143d18b23e56c9f146b2cb96b
tomw45362 <tom.m.weeks@gmail.com>
2015-09-01 11:24:29 -0400

    Fix issue with cookbook.sh, double quotes required for variable passthrough

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 cd /home/chef-repo
 dhclient -r && dhclient -H $HOSTNAME
 sleep 10
-knife bootstrap localhost -x root -P pass -r 'recipe[$1]' >> /var/log/knife.log
-knife bootstrap localhost -x root -P pass -r 'recipe[bootstrap]' >> /var/log/knife.log
+knife bootstrap localhost -x root -P pass -r "recipe[$1]" >> /var/log/knife.log
+knife bootstrap localhost -x root -P pass -r "recipe[bootstrap]" >> /var/log/knife.log
